ff8f3c7a0aa08dfd9dea75580ea9ada045ef6a38
nichro02 <nichro02@gmail.com>
2021-01-17 17:24:08 -0800

    Refactored code to OOP. User has ability to play through game in terminal

--- app.py
+++ app.py
@@ -3,17 +3,21 @@ import random
 #selected_word = ''
 word_bank = ['pizza', 'python', 'sleep', 'class']
 
+#get random word
 def get_random_word():
-    return random.choice(word_bank)
+    return random.choice(word_bank).upper()
     
-#get_random_word()
-
+#DECLARE WORD CLASS -----------------------------
 class Word():
     def __init__(self, chosen_word=None):
         self.chosen_word = get_random_word()
         pass # this method will split the word up into a list of dictionaries with 2 attributes:
         pass # the letter/character, and a boolean representing whether or not it has been guessed
     
+    def split_string(self):
+        word_to_list = list(self.chosen_word)
+        print(word_to_list)
+
     def print_word(self):
         print('Hit print word', self.chosen_word)
 
@@ -21,14 +25,40 @@ class Word():
         print('Hit check letter')
 
 answer = Word()
-#print(test.chosen_word)
+#test that random word gets selected and split into list
 answer.print_word()
+answer.split_string()
 
+#FUNCTIONS -------------------------------------
+def decrement_round():
+    print('starting round', game['guesses_left'])
+    game['guesses_left'] -= 1
+    print('rounds left,', game['guesses_left'])
+
+def game_flow_logic():
+    pass #inset game flow here
+    if game['guesses_left'] > 0:
+        guess = input('Please guess a letter ').upper()
+        #evaluate if guessed letter is in word
+        print(guess)
+        win_scenario()
+    else:
+        print('Too bad! You ran out of guesses before you could spell the word')
+
+
+def win_scenario():
+    if game['current_word'] == answer.chosen_word:
+        print('You have won the game')
+
+#GAME OBJECT -------------------------------------
 game = {
     'current_word': ' _ ' * len(answer.chosen_word),
     'guessed_letters': [],
-    'guesses_left': 8,
-    
+    'guesses_left': 8
+    #'decrement_round': decrement_round
 }
 
-print(game['current_word'])
+
+
+#decrement_round()
+game_flow_logic()
--- game.py
+++ game.py
@@ -1,42 +1,98 @@
 import random
 
-selected_word = ''
-word_bank = ['pizza', 'python', 'sleep', 'class']
-
-def get_random_word():
-    return random.choice(word_bank)
+#instantiate word in game class
 
 class Word():
     def __init__(self, chosen_word):
         self.chosen_word = chosen_word
+        self.chosen_word_info = []
+        for letter in chosen_word:
+            #create dictionary to hold letters of chosen word and a status to indicate whether word has been guessed
+            chosen_word_dictionary = {}
+            chosen_word_dictionary['letter'] = letter
+            chosen_word_dictionary['guessed'] = False
+            self.chosen_word_info.append(chosen_word_dictionary)
+
+    def get_chosen_word(self):
+        return self.chosen_word_info
+
+
+
+class Game():
+    #initialize game
+    def __init__(self):
+        self.rounds = 8
+        self.word_bank = ['pizza', 'python', 'sleep', 'class']
+        self.word = ''
+        self.word_info = []
+        self.guess = ''
+        self.current_word = ' _ '*len(self.word)
+        self.guessed_letter_count = 0
+    #get word to be guessed
+    def get_random_word(self):
+        #instatiate Word class with random word from with word_bank
+        get_word = Word(random.choice(self.word_bank).upper())
+        #store word_info dictionary to use to evaluate guesses
+        self.word_info = get_word.chosen_word_info
+        print('WORD INFO DICTIONARY: ',get_word.chosen_word_info)
+        #store the string
+        self.word = get_word.chosen_word
+        print('THE WORD IS...',self.word)
+        #start game
+        self.game_flow_logic()
+        #return self.word.chosen_word
     
-    def check_letter(self):
-        print('Hit check letter')
+    #game flow:
+    #prompt user to guess letter
+    #evaluate if guessed letter is part of word
+    #send correct/wrong guess message
+    #evaluate if winning criteria is met
+    #decrement round by 1
+    def game_flow_logic(self):
+        self.word_at_start_of_round()
+        self.guess = input(' Please guess a letter ').upper()
+        print(self.guess)
+        self.rounds -= 1
+        print(self.rounds)
+        self.print_word()
+
+    #show word at beginning of round
+    #populate any letters that have already been guessed
+    def word_at_start_of_round(self):
+        for item in self.word_info:
+            if item['guessed'] == True:
+                print(item['letter'], end='')
+            else:
+                print(' _ ',end='')
+
+    #print updated word after guess has been evaluated
+    def print_word(self):
+        for item in self.word_info:
+            #print(item)
+            if self.guess == item['letter']:
+                print(item['letter'], end='')
+                item['guessed'] = True
+                self.guessed_letter_count += 1
+                
+            elif item['guessed'] == True:
+                print(item['letter'], end='')
+            else:
+                print(' _ ',end='')
+        print(' # OF GUESSED LETTERS:', self.guessed_letter_count)
+        #evaluate win scenario
+        self.win_scenario()
+
+    #win game logic: # of guessed letters equals length of word being guessed
+    def win_scenario(self):
+        if self.guessed_letter_count == len(self.word):
+            print('Congrats! You won the game')
+        elif self.rounds > 0:
+            self.game_flow_logic()
+        else:
+            print('You lost the game')
 
 
-
-
-game = {
-    'current_word': ' _ ' * len(),
-    'guessed_letters': [],
-    'guesses_left': 8,
-    
-    def start_game():
-        #initiate game
-    
-
-    def show_scoreboard():
-        #display remaining letters
-        #display remaining turns
-    
-
-    def process_user_guess():
-        #GAME LOGIC
-
-        #check to see if word has been guessed, game is won
-
-        #check to see if guesses_left > 0
-
-        #else if guesses left = 0, game is over
-    
-}
+#instantiate game object    
+game = Game()
+#get random word to start game
+game.get_random_word()
